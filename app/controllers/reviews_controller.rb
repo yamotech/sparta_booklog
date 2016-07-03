5cb39194a5965dffc77895e0ecb14bad9dc0fc3a
yamotech <nothings.2c9@gmail.com>
2016-07-03 13:53:44 +0900

    ビューの作成

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,28 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def new
+    @book = Book.find(params[:book_id])
+    @review = Review.new
+  end
+
+  def create
+    @book = Book.find(params[:book_id])
+    @review = Review.new(
+      book_id: params[:book_id],
+      uzer_id: current_user.id,
+      body: review_params["body"]
+    )
+    if @review.save
+      redirect_to book_url(@review.book)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:body)
   end
 end
