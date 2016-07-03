756f3adc970ffa0d9882eb6b4023ee7a34fc5799
yamotech <nothings.2c9@gmail.com>
2016-07-03 12:16:10 +0900

    ルート開発

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :books
+
+  resources :books do
+    resources :reviews
+  end
+
   root 'books#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
